dc790f2b3e5e2445287b5ddfd9bf6efeb547db1f
liangwh <0027015537@iwhalecloud.com>
2020-08-31 10:50:48 +0800

    init

---
+++ src/main/java/xyz/liangwh/headwaters/HeadwatersApplication.java
@@ -0,0 +1,18 @@
+package xyz.liangwh.headwaters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("xyz.liangwh.headwaters.core.dao")
+@SpringBootApplication
+@Slf4j
+public class HeadwatersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadwatersApplication.class, args);
+        log.info("############################启动成功#########################");
+    }
+
+}
