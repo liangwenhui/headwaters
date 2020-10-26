2da712ea77c23e8d5e260ecac612273666d029e0
liangwh <0027015537@iwhalecloud.com>
2020-10-26 14:43:29 +0800

    修改为接受前端命令，解析命令获取指定key的seq(未完成)

--- src/main/java/xyz/liangwh/headwaters/netty/ServerHandler.java
+++ src/main/java/xyz/liangwh/headwaters/netty/ServerHandler.java
@@ -2,9 +2,13 @@ package xyz.liangwh.headwaters.netty;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+
 import org.apache.commons.lang.StringUtils;
 import xyz.liangwh.headwaters.core.interfaces.IDGenerator;
 import xyz.liangwh.headwaters.core.model.Result;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerHandler extends ChannelInboundHandlerAdapter {
 
@@ -19,15 +23,42 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 //        System.out.println("------->channel active");
     }
 
-    @Override
+
+    public static void main(String[] args) {
+        String command = "sequence     test   \n";
+        String[] commands = command.split(" ");
+        List<String> cs = new ArrayList<>();
+        String tmp ;
+        if(commands.length>1){
+            for(String arg:commands){
+                tmp = arg.trim();
+                if(StringUtils.isNotEmpty(tmp)){
+                    cs.add(tmp);
+                }
+            }
+        }
+        System.out.println(cs);
+    }
+//    private List<String> parse(String command){
+//        command = "sequence   test     \n";
+//
+//    }
+
+    @Override//sequence test\n
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 //        System.out.println("------->server channel read");
         if(msg!=null){
-            if(StringUtils.startsWith((String) msg,"seq")){
+            if(StringUtils.startsWith((String) msg,"sequence")){
                 Result test = idGenerator.getId("test");
-                ctx.write(test.getId()+"\n");
+                StringBuffer id = new StringBuffer();
+                id.append(test.getId());
+                ctx.write("$"+id.toString().length());
+                //ctx.write(id.toString().length());
+                ctx.write("\n");
+                ctx.write(id.toString());
+                ctx.write("\n");
             }else{
-                ctx.write("-Err");
+                ctx.write("-Error headwaters only suport command [sequence]\n");
             }
         }
         //System.out.println(ctx.channel().remoteAddress()+"----->Server :"+ msg.toString());
